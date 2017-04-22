bd0fce1f79d18860069742f351cf56da9395e974
Wasdns <952693358@qq.com>
2017-04-22 21:04:14 +0800

    update entry.c
    update entry.c

--- init/entry.c
+++ init/entry.c
@@ -1,10 +1,18 @@
 /* Entry.c */
 #include "types.h"
 #include "console.h"
+#include "string.h"
 
 int kern_entry() {
     console_clear();
-    console_write_color("HELLO MY FRIEND! MY NAME IS HURLEX97.\n", rc_black, rc_red);
-    console_write_color("MADE BY WASDNS!\n", rc_black, rc_red);
+    console_write_color("HELLO MY FRIEND! MY NAME IS HURLEX97.\n", rc_black, rc_light_red);
+    console_write_color("MADE BY WASDNS!\n", rc_black, rc_light_red);
+    uint32_t num = 100;
+
+    printk("\nTests:\n");
+    console_write_dec(num, rc_black, rc_light_blue);
+    console_write_hex(num, rc_black, rc_light_blue);
+
+    printk("\nThis is a function named printk, its value is %d\n", 1123);
     return 0;
 }
