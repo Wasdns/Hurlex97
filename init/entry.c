b6761a958d3ab8e733ae262c2db3f77be1af8bba
Wasdns <952693358@qq.com>
2017-05-02 20:54:31 +0800

    add debug
    add debug

--- init/entry.c
+++ init/entry.c
@@ -5,14 +5,22 @@
 
 int kern_entry() {
     console_clear();
+
+    printk("\n*************************************\n");
     console_write_color("HELLO MY FRIEND! MY NAME IS HURLEX97.\n", rc_black, rc_light_red);
     console_write_color("MADE BY WASDNS!\n", rc_black, rc_light_red);
-    uint32_t num = 100;
 
+    printk("\n*************************************\n");
+    uint32_t num = 100;
     printk("\nTests:\n");
     console_write_dec(num, rc_black, rc_light_blue);
     console_write_hex(num, rc_black, rc_light_blue);
 
     printk("\nThis is a function named printk, its value is %d\n", 1123);
+
+    printk("\n*************************************\n");
+    panic("test");
+    printk("\n*************************************\n");
+
     return 0;
 }
